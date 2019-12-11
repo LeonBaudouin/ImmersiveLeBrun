29fc088fd689bbd74bad3ddf3c67ec4b5551039e
LeonBaudouin <leondedouin@gmail.com>
2019-12-11 21:54:22 +0100

    Smoothed Camera mouse follow

--- src/classes/Controller/CameraMouseFollow.ts
+++ src/classes/Controller/CameraMouseFollow.ts
@@ -4,23 +4,38 @@ import * as THREE from 'three'
 
 export default class CameraMouseFollow extends AbstractController {
     private mouseMoveListener: MouseMoveListener
-    private lastMouse: THREE.Vector2 = new THREE.Vector2(0, 0)
-    private speed: THREE.Vector2 = new THREE.Vector2(0.5, 0.5)
+    private lastMouse: THREE.Vector2
+    private speed: THREE.Vector2
+    private maxRotation: THREE.Vector2
 
-    constructor() {
+    constructor(
+        firstMousePosition: THREE.Vector2 = new THREE.Vector2(0, 0),
+        speed: THREE.Vector2 = new THREE.Vector2(0.07, 0.07),
+        maxRotation: THREE.Vector2 = new THREE.Vector2(0.02, 0.02),
+    ) {
         super()
+        this.lastMouse = firstMousePosition
+        this.speed = speed
+        this.maxRotation = maxRotation
         this.mouseMoveListener = MouseMoveListener.getInstance()
     }
 
     update(object3d: THREE.Object3D, time: number) {
         const mouse = this.mouseMoveListener.getValue()
         if (mouse) {
-            this.lastMouse.set(mouse.x, mouse.y)
-            // this.lastMouse.add(mouse.sub(this.lastMouse)).multiply(this.speed)
+            const mouseX =
+                this.lastMouse.x + (mouse.x - this.lastMouse.x) * this.speed.x
+            const mouseY =
+                this.lastMouse.y + (mouse.y - this.lastMouse.y) * this.speed.y
+
+            this.lastMouse.set(mouseX, mouseY)
+
             object3d.rotation.y =
-                (1 - (2 * this.lastMouse.x) / window.innerWidth) * 0.05
+                (1 - (2 * this.lastMouse.x) / window.innerWidth) *
+                this.maxRotation.x
             object3d.rotation.x =
-                (1 - (2 * this.lastMouse.y) / window.innerHeight) * 0.05
+                (1 - (2 * this.lastMouse.y) / window.innerHeight) *
+                this.maxRotation.y
         }
     }
 }
