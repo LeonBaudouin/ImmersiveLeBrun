1090f3f76ab765bc3ed1f59e091cd2e6568c7341
LeonBaudouin <43001396+LeonBaudouin@users.noreply.github.com>
2020-03-10 00:54:19 +0100

    :sound: Added sounds

--- src/setup.ts
+++ src/setup.ts
@@ -19,6 +19,7 @@ import ComposerScene from './classes/Core/ComposerScene'
 import FadeController from './classes/Controller/FadeController'
 import Key from './classes/Key'
 import TextureLoader from './classes/Core/TextureLoader'
+import AudioLoader from './classes/Core/AudioLoader'
 
 function initWebglRenderer(camera: THREE.Camera): THREE.WebGLRenderer {
     const renderer = new THREE.WebGLRenderer({
@@ -41,6 +42,19 @@ function initCSS3DRenderer(camera: THREE.Camera): RendererInterface {
     // controls.maxDistance = 1500
     // controls.minDistance = 0
     return renderer
+}
+
+function audio(
+    audioListener: THREE.AudioListener,
+    buffer: THREE.AudioBuffer,
+    volume: number = 1,
+    loop: boolean = true,
+) {
+    const audio = new THREE.Audio(audioListener)
+    audio.setBuffer(buffer)
+    audio.setLoop(loop)
+    audio.setVolume(volume)
+    return audio
 }
 
 function waitAnim(): Promise<void> {
@@ -182,9 +196,25 @@ export default function Setup(key: Key): Promise<{ raf: Function; cb: Function }
     TextureLoader.setLoadingManager(new THREE.LoadingManager())
     TextureLoader.addOnProgressCallback(key.updateProgress.bind(key))
 
+    const audioListener = new THREE.AudioListener()
+    camera.add(audioListener)
+
     return waitAnim()
         .then(() => Promise.all([room.loadRoom(), room2.loadRoom(), room3.loadRoom()]))
-        .then(() => {
+        .then(() =>
+            AudioLoader.load(
+                {
+                    spring: 'spring.mp3',
+                    workshop_ambient: 'workshop_ambient.mp3',
+                    quote: 'quote.mp3',
+                    quote_alt: 'quote_alt.mp3',
+                    reveal: 'reveal.mp3',
+                    reveal_alt: 'reveal_alt.mp3',
+                },
+                './assets/audio/',
+            ),
+        )
+        .then((buffers: { [name: string]: THREE.AudioBuffer }) => {
             EventEmitter.getInstance().Emit(EVENT.INTERACTIVE_BIND, scenes[0].name)
             return {
                 raf: () => {
@@ -196,6 +226,42 @@ export default function Setup(key: Key): Promise<{ raf: Function; cb: Function }
                 cb: () => {
                     const mouse = new THREE.Vector2()
 
+                    const springAudio = audio(audioListener, buffers.spring, 0.03, true)
+                    springAudio.play()
+                    const workshopAmbient = audio(audioListener, buffers.workshop_ambient, 0.1, true)
+                    workshopAmbient.play()
+
+                    const quoteSound = audio(audioListener, buffers.quote, 0.3, false)
+                    const quoteAltSound = audio(audioListener, buffers.quote_alt, 1.2, false)
+                    const revealAltSound = audio(audioListener, buffers.reveal_alt, 0.2, false)
+                    revealAltSound.setPlaybackRate(0.8)
+                    const revealSound = audio(audioListener, buffers.reveal, 0.2, false)
+                    revealSound.setPlaybackRate(1.3)
+
+                    EventEmitter.getInstance().Subscribe(EVENT.OPEN_QUOTE, () => {
+                        if (Math.random() > 0.5) {
+                            quoteSound.setPlaybackRate(Math.random() * 0.6 + 0.7)
+                            quoteSound.play()
+                        } else {
+                            quoteAltSound.play()
+                        }
+                    })
+
+                    EventEmitter.getInstance().Subscribe(
+                        EVENT.INTERACTIVE_CLICK,
+                        ({ firstClick, component: object3d }) => {
+                            if (firstClick && object3d.userData.name != null) {
+                                if (revealSound.isPlaying) revealSound.stop()
+                                if (revealAltSound.isPlaying) revealAltSound.stop()
+                                if (Math.random() > 0.5) {
+                                    revealSound.play()
+                                } else {
+                                    revealAltSound.play()
+                                }
+                            }
+                        },
+                    )
+
                     key.addKeyCb(() => {
                         document.querySelector('.loading-screen').classList.remove('close')
                         document.querySelector('.loading-screen').classList.add('open')
@@ -203,7 +269,6 @@ export default function Setup(key: Key): Promise<{ raf: Function; cb: Function }
                         started = true
 
                         // -- Raycast --
-
                         document.addEventListener('mousemove', e => {
                             const { clientX, clientY } = e
                             mouse.x = (clientX / window.innerWidth) * 2 - 1
@@ -211,6 +276,11 @@ export default function Setup(key: Key): Promise<{ raf: Function; cb: Function }
                             Raycaster.getInstance().Cast(camera, mouse)
                             MouseMoveListener.getInstance().UpdateValue(e)
                         })
+
+                        EventEmitter.getInstance().Subscribe(
+                            EVENT.INTERACTIVE_MOUSEENTER,
+                            () => (document.body.style.cursor = 'pointer'),
+                        )
 
                         EventEmitter.getInstance().Subscribe(
                             EVENT.INTERACTIVE_MOUSEENTER,
